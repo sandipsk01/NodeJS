e1e758bcee8bfe82aad260a83c9f144689fb1805
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-10-27 22:53:10 +0530

    MVC Project1: Controller, Model

--- 05 MVC-1/InventoryManager/src/controllers/product.controller.js
+++ 05 MVC-1/InventoryManager/src/controllers/product.controller.js
@@ -1,7 +1,9 @@
 import path from 'path';
+import ProductModel from '../models/product.model.js';
 
 export default class ProductController{
     getProducts(req, res){
+        let products = ProductModel.get();
         return res.sendFile(path.join(path.resolve(), 'src', 'views', 'products.html'))
     }
 }
