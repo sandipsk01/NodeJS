79cb4c83c767c7c6a3d308d4613b9dfb7a4bb4ba
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-11-22 20:41:40 +0530

    express.Router

--- 09 REST API/E-COM API/server.js
+++ 09 REST API/E-COM API/server.js
@@ -1,8 +1,12 @@
 // 1. Import Express
 import express from 'express';
+import * as ProductRouter from './src/features/product/product.routes.js';
 
 // 2. Create Server
 const server = express();
+
+// for all requests related to product, redirect to product routes
+server.use('/api/products', ProductRouter);
 
 // 3. Default Requiest Handler
 server.get("/", (req,res)=>{
